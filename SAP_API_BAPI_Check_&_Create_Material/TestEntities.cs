7f875c06abd6d3e9578cac31c15568f0055269b7
Demo <don.jackson@microfocus.com>
2022-04-15 17:46:55 +0000

    Initial commit of SAP BAPI API test

---
+++ SAP_API_BAPI_Check_&_Create_Material/TestEntities.cs
@@ -0,0 +1,40 @@
+using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using HP.ST.Fwk.RunTimeFWK.Utilities;
+    using HP.ST.Fwk.RunTimeFWK.BindingFWK;
+    
+    namespace Script
+    {
+    
+    public class TestEntities
+    {
+    public ISTRunTimeContext Context = null;
+    public Dictionary<string, HP.ST.Fwk.RunTimeFWK.DataHandling.IDataSource> dataSourceNameToDataSource = new Dictionary<string, HP.ST.Fwk.RunTimeFWK.DataHandling.IDataSource>();
+    
+    protected HP.ST.Fwk.RunTimeFWK.DataHandling.IDataSource GetDataSource(string dataSourceName)
+    {
+    if(!dataSourceNameToDataSource.ContainsKey(dataSourceName))
+    	throw new Exception(("A data source with the specified name does not exist."));
+    return dataSourceNameToDataSource[dataSourceName];
+    }
+    public HP.ST.Ext.BasicActivities.DataFetchActivity DataFetchActivity12 = null;
+    public HP.ST.Ext.BasicActivities.StartActivity StartActivity1 = null;
+    public HP.ST.Fwk.RunTimeFWK.CompositeActivities.Loop<Loop2Input> Loop2 = null;
+    public HP.ST.Ext.BasicActivities.DataExporterActivity DataExporterActivity14 = null;
+    public HP.ST.Ext.BasicActivities.DataExporterCloseActivity DataExporterCloseActivity15 = null;
+    public HP.ST.Ext.BasicActivities.EndActivity EndActivity3 = null;
+    public HP.ST.Ext.BasicActivities.DataDisconnectActivity DataDisconnectActivity13 = null;
+    public HP.ST.Fwk.RunTimeFWK.CompositeActivities.Sequence Sequence11 = null;
+    public HP.ST.Ext.SapRT.Activities.RfcCallActivityRT RfcCallActivityRT7 = null;
+    public HP.ST.Fwk.RunTimeFWK.CompositeActivities.IfElse<IfElse4Input> IfElse4 = null;
+    public HP.ST.Fwk.RunTimeFWK.CompositeActivities.IfElseBranch IfElseBranch5 = null;
+    public HP.ST.Fwk.RunTimeFWK.CompositeActivities.IfElseBranch IfElseBranch6 = null;
+    public HP.ST.Ext.SapRT.Activities.RfcCallActivityRT RfcCallActivityRT8 = null;
+    public HP.ST.Ext.SapRT.Activities.RfcCallActivityRT RfcCallActivityRT9 = null;
+    public HP.ST.Ext.SapRT.Activities.RfcCallActivityRT RfcCallActivityRT10 = null;
+    
+    }
+    
+    }
+    
---
+++ SAP_API_BAPI_Check_&_Create_Material/TestUserCode.cs
@@ -0,0 +1,19 @@
+namespace Script
+{
+    using System;
+    using System.Xml;
+    using System.Xml.Schema;
+    using HP.ST.Ext.BasicActivities;
+    using HP.ST.Fwk.RunTimeFWK;
+    using HP.ST.Fwk.RunTimeFWK.ActivityFWK;
+    using HP.ST.Fwk.RunTimeFWK.Utilities;
+    using HP.ST.Fwk.RunTimeFWK.CompositeActivities;
+	using HP.ST.Ext.CustomDataProviders.Extensions;
+	using HP.ST.Ext.CustomDataProviders.ExcelFileArguments;
+    
+    [Serializable()]
+    public class TestUserCode : TestEntities
+    {
+    }
+
+}
